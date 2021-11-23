46aa1be2d81b4a2656307ff9f84ecfcd31626503
Juanels1918 <jeoviedos@correo.udistrital.edu.co>
2021-11-23 11:50:26 -0500

    Correción y creación de carpetas/Contrtolador

---
+++ src/controlador/Controlador.java
@@ -0,0 +1,12 @@
+package controlador;
+
+/**
+ * Clase y método principal
+ * @author Laura Villalobos / Juan Oviedo / David Alvarez
+ * */
+public class Controlador {
+
+	public static void Main(String args[]) {
+		
+	}
+}
