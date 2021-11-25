286e11d71f3b04bdb2deba60f74058a5fe896eb2
ELFish998 <lv.villalobos1998@gmail.com>
2021-11-24 19:54:36 -0500

    espectador
    contructor y getters and setters

--- src/Modelo/Espectador.java
+++ src/Modelo/Espectador.java
@@ -8,6 +8,46 @@ public class Espectador {
     private String nombre;
     private int edad;
     private int dinero;
-    Boleto B;
+    Boleto b; 
+    
+    public Espectador(String nombre, int edad, int dinero, Boleto b)
+    {
+    	this.nombre = nombre;
+    	this.edad = edad;
+    	this.dinero = dinero;
+    	this.b = b;
+    }
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getDinero() {
+		return dinero;
+	}
+
+	public void setDinero(int dinero) {
+		this.dinero = dinero;
+	}
+
+	public Boleto getB() {
+		return b;
+	}
+
+	public void setB(Boleto b) {
+		this.b = b;
+	}
 
 }
